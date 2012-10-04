0ae622b5dd66b40faa55242cb9a85d86fe3ab3d4
Marc Adam <marc.m.adam@gmail.com>
2012-10-04 15:25:06 -0400

    added final version of sf2float and README

--- sf2float.c
+++ sf2float.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "portsf.h"
-#include <string.h>
+#include <math.h>
 
 int main(int argc, char* argv[])
 {
@@ -17,86 +17,24 @@ int main(int argc, char* argv[])
 	PSF_CHPEAK* peaks = NULL;
 	float* frame = NULL;
 	psf_format outformat =  PSF_FMT_UNKNOWN;
-  
-  /*
-   * variables to be used in my customized program
-   */
-  int multiplier = 1;
-  int pan = 0.5;
-  int env_interval = 50; // in milliseconds
 
 	printf("SF2FLOAT: convert soundfile to 32bit floats format\n");
 
-	if(argc < 3){
+	if(argc < 4){
 		printf("insufficient arguments.\n"
 				"usage:\n\t"
-				"audiodize infile outfile\n");
+				"sf2float option infile outfile\n");
 		return 1;
 	}
-
-  /*
-   * created variables for saving mods
-   */
-  int mode_m = 0;
-  int mode_sm = 0;
-  int mode_ltr = 0;
-  int mode_pan = 0;
-  int mode_env = 0;
-
-  /*
-   * for lack of a good argument parser,
-   * odd arguments are argument types,
-   * even arguments are argument values.
-   */
-  int infileIndex = 0;
-  int outfileIndex = 0;
-
-  for (i = 1; i < argc-1; i+=2) {
-    if (!strcmp(argv[i],"-m")) {
-      multiplier = atoi(argv[i+1]);
-      mode_m = 1;
-    }
-    if (!strcmp(argv[i],"-sm")) {
-      multiplier = atoi(argv[i+1]);
-      mode_sm = 1;
-    }
-    if (!strcmp(argv[i],"-pan")) {
-      pan = atoi(argv[i+1]);
-      mode_pan = 1;
-    }
-    if (!strcmp(argv[i],"-env")) {
-      env_interval = atoi(argv[i+1]);
-      mode_env= 1;
-    }
-    if (!strcmp(argv[i],"-ltr")) {
-      mode_ltr = 1;
-      i--;
-    }
-
-    if (!strcmp(argv[i], "-i")) {
-      infileIndex = i+1;
-    }
-    if (!strcmp(argv[i], "-o")) {
-      outfileIndex = i+1;
-    }
-
-  }
-  printf("input file: %s", argv[infileIndex]);
-  printf("output file: %s", argv[outfileIndex]);
-
 	/* be good, and startup portsf */
 	if(psf_init()){
 		printf("unable to start portsf\n");
 		return 1;
 	}
-  if(infileIndex==0 || outfileIndex==0) {
-    printf("Error: infile or outfile not specified\n");
-    return 1;
-  }
 	
-	ifd = psf_sndOpen(argv[infileIndex],&props,0);																		  
+	ifd = psf_sndOpen(argv[2],&props,0);																		  
 	if(ifd < 0){
-		printf("Error: unable to open infile %s\n",argv[1]);
+		printf("Error: unable to open infile %s\n",argv[2]);
 		return 1;
 	}
 
@@ -117,19 +55,42 @@ int main(int argc, char* argv[])
 	}
 	props.samptype = PSF_SAMP_IEEE_FLOAT;
 	/* check file extension of outfile name, so we use correct output file format*/
-	outformat = psf_getFormatExt(argv[outfileIndex]);
-	if(outformat == PSF_FMT_UNKNOWN && !(mode_env==1)){
-		printf("outfile name %s has unknown format.\n"
-			"Use any of .wav, .aiff, .aif, .afc, .aifc\n",argv[2]);
-		error++;
-		goto exit;
-	}
+
+  // TODO: needs to be changed because output is not always the same format
+  if (atoi(argv[1])<3) {
+    outformat = psf_getFormatExt(argv[3]);
+    printf("output file format: %d", outformat);
+    if(outformat == PSF_FMT_UNKNOWN){
+      printf("outfile name %s has unknown format.\n"
+        "Use any of .wav, .aiff, .aif, .afc, .aifc\n",argv[3]);
+      error++;
+      goto exit;
+    }
+  }
+  else if (atoi(argv[1])==4) {
+    outformat = psf_getFormatExt(argv[4]);
+    printf("output file format: %d", outformat);
+    if(outformat == PSF_FMT_UNKNOWN){
+      printf("outfile name %s has unknown format.\n"
+        "Use any of .wav, .aiff, .aif, .afc, .aifc\n",argv[4]);
+      error++;
+      goto exit;
+    }
+  }
 	props.format = outformat;
 
-  if(mode_env!=1) {
-    ofd = psf_sndCreate(argv[outfileIndex],&props,0,0,PSF_CREATE_RDWR);
+	if(atoi(argv[1])<3) {
+    ofd = psf_sndCreate(argv[3],&props,0,0,PSF_CREATE_RDWR);
     if(ofd < 0){
-      printf("Error: unable to create outfile %s\n",argv[2]);
+      printf("Error: unable to create outfile %s\n",argv[3]);
+      error++;
+      goto exit;
+    }
+  }
+  else if (atoi(argv[1])==4) {
+    ofd = psf_sndCreate(argv[4],&props,0,0,PSF_CREATE_RDWR);
+    if(ofd < 0){
+      printf("Error: unable to create outfile %s\n",argv[4]);
       error++;
       goto exit;
     }
@@ -156,145 +117,185 @@ int main(int argc, char* argv[])
 	totalread = 0;		/* count sample frames as they are copied */
 
 
+  int part = atoi(argv[1]);
 
   /*
-   * My own code begins.
+   * Switch depending on the part of the assignment
    */
+  switch(part) {
 
-  /*
-   * for pre processing, do a first visit over the file
-   */
-
-  /*
-   * mode: multiplication (-m or -sm)
-   */
-  if(mode_m==1 || mode_sm==1) {
-    while(framesread == 1) {
-      double xx = frame[0];
-      double yy = frame[1];
-      frame[0] = frame[0]*multiplier;
-      frame[1] = frame[1]*multiplier;
-      printf("%f %f\t%f %f\n", xx, yy, frame[0], frame[1]);
-      
-      totalread++;
-      if(psf_sndWriteFloatFrames(ofd,frame,1) != 1){
-          printf("error writing to outfile\n");
-          error++;
-          break;
-      }
-      framesread = psf_sndReadFloatFrames(ifd,frame,1);
-    }
-  }
-  if(mode_ltr==1) {
-    while(framesread == 1) {
-      frame[1] = frame[0];
-      printf("%f %f\n", frame[0], frame[1]);
-      
-      totalread++;
-      if(psf_sndWriteFloatFrames(ofd,frame,1) != 1){
-          printf("error writing to outfile\n");
-          error++;
-          break;
-      }
-      framesread = psf_sndReadFloatFrames(ifd,frame,1);
-    }
-  }
-
-  /*
-   * mode: panning (-pan)
-   */
-  else if(mode_pan==1) {
-    int cycleSampleSize = pan*44100;
-    // when left is going up and right is going down
-    int cycle1Limit = cycleSampleSize/4;
-    // when left is going down and right is going up
-    int cycle4Limit = 3*cycleSampleSize/4;
-    int currentSample = 0;
-
-    int sampleCount = 0;
-    double panFactor = 1;
-    
-    while (framesread == 1){
-      frame[0]*=panFactor;
-      frame[1]/=panFactor;
-      if (currentSample<=cycle1Limit || currentSample>cycle4Limit) {
-        panFactor+=0.000015;
-      }
-      else {
-        panFactor-=0.000015;
-      }
-      // printf("%f\n", panFactor);
-      totalread++;
-      currentSample++;
-      if(currentSample==pan*44100) {
-        currentSample = 0;
-      }
-      if(psf_sndWriteFloatFrames(ofd,frame,1) != 1){
-          printf("error writing to outfile\n");
-          error++;
-          break;
-      }
-      framesread = psf_sndReadFloatFrames(ifd,frame,1);
-    }
-  }
-  /*
-   * envelop creator mode (-env)
-   */
-  else if(mode_env==1) {
-    FILE *file;
-    file = fopen(argv[outfileIndex], "w+");
-    double sampleInterval = 44100*(double)env_interval/1000;
-    double currentSample = 0;
-    double max = 0;
-    double n =0;
-    while (framesread ==1) {
-      if(currentSample < sampleInterval) {
-        if(max<frame[0]) {
-          max=frame[0];
+    /*
+     * Part 0: Output the file as is.
+     */
+    case 0:
+      while (framesread == 1){
+        totalread++;
+        printf("%f %f\n", frame[0], frame[1]);
+        if(psf_sndWriteFloatFrames(ofd,frame,1) != 1){
+            printf("Error writing to outfile\n");
+            error++;
+            break;
         }
-        if (max<frame[1]) {
-          max=frame[1];
-        }
-        currentSample++;
+        framesread = psf_sndReadFloatFrames(ifd,frame,1);
       }
-      else {
-        currentSample = 0;
-        fprintf(file,"%f %f\n", (double)(n*sampleInterval), max);
-        max = 0;
-        n++;
-      }
-      totalread++;
-      if(currentSample==pan*44100) {
-        currentSample = 0;
-      }
-      
-      framesread = psf_sndReadFloatFrames(ifd,frame,1);
-    }
-    fclose(file);
+      break;
+
+    /*
+     * Part 1: Divide the value of each sample by 2.
+     */
+    case 1: {
+              while (framesread == 1){
+                frame[0] = frame[0]/2;
+                frame[1] = frame[1]/2;
+                totalread++;
+                if(psf_sndWriteFloatFrames(ofd,frame,1) != 1){
+                    printf("Error writing to outfile\n");
+                    error++;
+                    break;
+                }
+                framesread = psf_sndReadFloatFrames(ifd,frame,1);
+              }
+            break;
+            }
+
+    /*
+     * Part 2: Pan the output from left to right, with an interval of 5 seconds.
+     */
+    case 2: {
+              double cycle = 5; // cycle of 5 seconds
+              double period = 4 * cycle; // sine or cosine period
+              double sampleLimit = period * 44100;
+              while (framesread == 1){
+                frame[0] = frame[0]*sin(2*M_PI*totalread/sampleLimit);
+                frame[1] = frame[1]*cos(2*M_PI*totalread/sampleLimit);
+
+                totalread++;
+                if(psf_sndWriteFloatFrames(ofd,frame,1) != 1){
+                    printf("Error writing to outfile\n");
+                    error++;
+                    break;
+                }
+                framesread = psf_sndReadFloatFrames(ifd,frame,1);
+              }
+              break;
+            }
+    /*
+     * Part 3: Create envelope file.
+     */
+    case 3:
+            {
+              double max = 0; // to keep track of the max in the interval
+              int currentSample = 0;
+              double intervalTime = 0.05;
+              double sampleLimit = 44100*intervalTime;
+              int interval = 1;
+              FILE *file;
+              file = fopen(argv[3], "w+");
+              printf("sampleLimit %f\n", sampleLimit);
+              while (framesread == 1){
+                if (currentSample < sampleLimit) {
+                  if (max < frame[0]) {
+                    max = frame[0];
+                  }
+                  if (max < -1*frame[0]) {
+                    max = -1*frame[0];
+                  }
+                  if (max < frame[1]) {
+                    max = frame[1];
+                  }
+                  if (max < -1*frame[1]) {
+                    max = -1*frame[1];
+                  }
+                  currentSample++;
+                }
+                else {
+                  fprintf(file, "%f %f\n", (double)(interval*intervalTime), max);
+                  max = 0;
+                  currentSample = 0;
+                  interval++;
+                }
+
+                totalread++;
+                framesread = psf_sndReadFloatFrames(ifd,frame,1);
+              }
+              fclose(file);
+              break;
+            }
+    case 4:
+            {
+              // open up file, put the reading index on the first line
+              FILE *file;
+              file = fopen(argv[3], "r");
+              double intervalTime = 0.05;
+
+              char intervalLimitString[20], valueString[20], space[1];
+              double intervalLimit=-1, value=0;
+              // to do the linear regression and get the factor
+              double prevValue = 0;
+              double slope = 0;
+              double intercept = 0;
+              double factor = 1;
+              double current=0;
+
+              while (framesread == 1) {
+                if (!feof(file)) {
+                  if(current/44100 >= intervalLimit ) {
+                    if(fgets(intervalLimitString, 10, file) != NULL) {
+                      intervalLimit = strtod(intervalLimitString, NULL);
+                    }
+                    if(fgets(space, 1, file) != NULL) {}
+                    if(fgets(valueString, 12, file) != NULL) {
+                      prevValue = value;
+                      value = strtod(valueString, NULL);
+                      slope = (value-prevValue)/intervalTime;
+                      intercept = prevValue;
+                    }
+                  }
+                  factor = ((double)(totalread%((int)(44100*intervalTime)))/44100)*slope + intercept;
+                  frame[0] = frame[0]*factor;
+                  frame[1] = frame[1]*factor;
+                  int tt = totalread%2205;
+                  // printf("time: %d \t value: %f  \t slope %f \t intercep %f \t will multiply by: %f\n", tt, value, slope, intercept, factor);
+
+                }
+                current++;
+                totalread++;
+                if(psf_sndWriteFloatFrames(ofd,frame,1) != 1){
+                    printf("Error writing to outfile\n");
+                    error++;
+                    break;
+                }
+                framesread = psf_sndReadFloatFrames(ifd,frame,1);
+              }
+              break;
+            }
+    default:
+      printf("default case");
+      break;
   }
 
-
-  /*
-   * My own code ends.
-   */
 	if(framesread < 0)	{
 		printf("Error reading infile. Outfile is incomplete.\n");
 		error++;
 	}
-	else
-		printf("Done. %ld sample frames copied to %s\n",totalread,argv[2]);
-	/* report PEAK values to user */
-  if(mode_env!=1) {
-    if(psf_sndReadPeaks(ofd,peaks,NULL) > 0){
-      long i;
-      double peaktime;
-      printf("PEAK information:\n");	
-        for(i=0;i < props.chans;i++){
-          peaktime = (double) peaks[i].pos / (double) props.srate;
-          printf("CH %ld:\t%.4f at %.4f secs\n", i+1, peaks[i].val, peaktime);
-        }
-    }	
+	else {
+    if (atoi(argv[1])<3) {
+      printf("Done. %ld sample frames copied to %s\n",totalread,argv[3]);
+    }
+    else if (atoi(argv[1])==4) {
+      printf("Done. %ld sample frames copied to %s\n",totalread,argv[4]);
+    }
   }
+	/* report PEAK values to user */
+	if(psf_sndReadPeaks(ofd,peaks,NULL) > 0){
+		long i;
+		double peaktime;
+		printf("PEAK information:\n");	
+			for(i=0;i < props.chans;i++){
+				peaktime = (double) peaks[i].pos / (double) props.srate;
+				printf("CH %ld:\t%.4f at %.4f secs\n", i+1, peaks[i].val, peaktime);
+			}
+	}	
 	/* do all cleanup  */    
 exit:	 	if(ifd >= 0)
 		psf_sndClose(ifd);
